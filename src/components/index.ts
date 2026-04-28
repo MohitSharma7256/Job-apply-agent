89582775acbe3272f0bb1c6c2dc1ec8d7151b69d
Mohit Sharma <140817367+MohitSharma7256@users.noreply.github.com>
2026-04-28 11:42:46 +0000

    Add barrel exports for components to fix Render build issues

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { ActivityFeed } from './ActivityFeed';
+export { ApplyProgress } from './ApplyProgress';
+export { JobDetailView } from './JobDetailView';
+export { PlatformGrid } from './PlatformGrid';
+export { StatsDashboard } from './StatsDashboard';
+export * from './ui';
